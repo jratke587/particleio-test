adc144850de36bf7baa4cf4771c83c185b1296f8
jratke587 <58532458+jratke587@users.noreply.github.com>
2023-08-28 17:40:52 +0000

    add analogWrite

--- blink/src/blink.ino
+++ blink/src/blink.ino
@@ -15,9 +15,22 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  digitalWrite(D3, HIGH);
-  delay(500);
-  digitalWrite(D3, LOW);
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(D3, HIGH);
+    delay(500);
+    digitalWrite(D3, LOW);
+    delay(500);
+  }
+  for (int x = 0; x < 2; x++) {
+    for (int i=0; i<255; i++) {
+      analogWrite(D3, i);
+      delay(3.92);
+    }
+    for (int i=255; i>0; i--) {
+      analogWrite(D3, i);
+      delay(3.92);
+    }
+  }
   delay(500);
   uint32_t analogValue = analogRead(A0);
   Particle.publish(String(analogValue));
