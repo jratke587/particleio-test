719fe356d8c4b3befbf16841fe7a7448cfe1131f
jratke587 <58532458+jratke587@users.noreply.github.com>
2023-08-29 18:13:01 +0000

    changed functionality to toggle between two LEDs

--- button/src/button.ino
+++ button/src/button.ino
@@ -1,21 +1,21 @@
+bool ledSwitch = true;
+
 void setup() {
   pinMode(D5, INPUT_PULLDOWN);
   pinMode(D3, OUTPUT);
+  pinMode(D7, OUTPUT);
+  digitalWrite(D3, ledSwitch);
+  digitalWrite(D7, !ledSwitch);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   if (digitalRead(D5)) {
-    for (int i = 0; i < 10; i++) {
-      delay(50);
-      digitalWrite(D3, LOW);
-      delay(50);
-      digitalWrite(D3, HIGH);
-    }
-  } else {
-    digitalWrite(D3, HIGH);
-    delay(500);
-    digitalWrite(D3, LOW);
-    delay(500);
+    ledSwitch = !ledSwitch;
+    digitalWrite(D3, ledSwitch);
+    digitalWrite(D7, !ledSwitch);
+  }
+  while (digitalRead(D5)) {
+    delay(50);
   }
 }
