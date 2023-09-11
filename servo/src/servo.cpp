f793d32e2e0fb0aec6f2005b03911f6c3b3e0913
jratke587 <58532458+jratke587@users.noreply.github.com>
2023-09-11 18:09:14 +0000

    updated some values

---
+++ servo/src/servo.cpp
@@ -0,0 +1,26 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/workspaces/particleio-test/servo/src/servo.ino"
+void setup();
+void loop();
+#line 1 "/workspaces/particleio-test/servo/src/servo.ino"
+Servo myServo;
+int servoPin = D2;
+int potentiometer = A0;
+
+void setup() {
+  myServo.attach(servoPin);
+  pinMode(A0, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int val = analogRead(potentiometer);
+  int angle = map(val, 0, 4096, 10, 170); //map the potentiometer value to an angle from 0 to 180 degrees
+  Serial.println("> " + String(angle));
+  myServo.write(angle);
+  delay(20);
+}
--- servo/src/servo.ino
+++ servo/src/servo.ino
@@ -10,8 +10,8 @@ void setup() {
 
 void loop() {
   int val = analogRead(potentiometer);
-  int angle = map(val, 0, 4096, 0, 180); //map the potentiometer value to an angle from 0 to 180 degrees
-  Serial.println("> " + String(angle));
+  int angle = map(val, 0, 4096, 10, 170); //map the potentiometer value to an angle from 0 to 180 degrees
+  Serial.println("> " + String(val));
   myServo.write(angle);
-  delay(250);
+  delay(100);
 }
